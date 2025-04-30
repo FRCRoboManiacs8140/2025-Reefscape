dff4cb719d7d02d5106816f6ecd8e99f82adbd48
FRCRoboManiacs8140 <mc2stemfablab@outlook.com>
2025-04-30 13:45:51 -0400

    Remove controlls from drive subsystem, will eventually create controlls subsystem

--- src/main/java/frc/robot/Subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/DriveSubsystem.java
@@ -61,22 +61,6 @@ public class DriveSubsystem extends SubsystemBase {
         leftBack.set(0);
         rightBack.set(0);
     }
-    public static void controlls(){
-        if (drive_controller.getLeftBumper()) {
-            NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(2);
-            
-            if (tv == 1) {
-              DriveSubsystem.drive(
-                drive_controller.getLeftY()*-.3, 
-                -MathUtil.clamp(strafeController.calculate(tx, 0), -.5, .5), 
-                -MathUtil.clamp(turnController.calculate((gyro.getAngle() + offset) % 360, tagAngle),-.1,.1),
-                gyroangle
-                );
-             
-            }
-        }
-        
-    }
     
     public static void SmartDashboardDrive(){
      SmartDashboard.putNumber("Front Left", leftFront().get());
